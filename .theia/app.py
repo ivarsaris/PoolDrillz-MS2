454ddf1143e263938fc0014c6b049cfe776dbae0
Ivar Saris <ivarsaris@gmail.com>
2019-11-11 09:13:40 +0000

    connected exercises.html to mongoDb database

--- .theia/app.py
+++ .theia/app.py
@@ -1,7 +1,14 @@
 import os
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'pooldrillz'
+app.config["MONGO_URI"] = 'mongodb+srv://ivars:@cluster0-q4qh1.mongodb.net/pooldrillz?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 # opens index.html
 @app.route('/')
@@ -11,17 +18,18 @@ def index():
 # opens exercises.html
 @app.route('/exercises')
 def exercises():
-    return render_template('exercises.html')
+    return render_template('exercises.html', exercises=mongo.db.exercises.find())
 
 # opens stats.html
 @app.route('/stats')
 def stats():
     return render_template('stats.html')
 
-#opens contact.html
+# opens contact.html
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
 
 # opens port for browser
 if __name__ == "__main__":
