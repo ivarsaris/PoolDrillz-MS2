bb0a0b5b420dcf6ab071f92f4ba0c2009efa5efe
Ivar Saris <ivarsaris@gmail.com>
2019-11-21 02:19:12 +0000

    added cancel buttons and icons to all buttons

--- .theia/app.py
+++ .theia/app.py
@@ -82,7 +82,6 @@ def edit_exercise(exercise_id):
     # return edit template with info of the exercise filled out
     return render_template('editexercise.html', exercise=the_exercise)
 
-
 # route for update exercise function
 @app.route('/update_exercise/<exercise_id>', methods=["POST"])
 # update exercise function
@@ -95,10 +94,11 @@ def update_exercise(exercise_id):
         # all variables to be filled out in the form
         'name': request.form.get('name'),
         'description': request.form.get('description'),
-        'type_of_exercise': request.form.get('type_of_exercise'),
+        'type_of_exercise': request.form.getlist('type_of_exercise'),
         'skill_level': request.form.get('skill_level'),
         'date_added': request.form.get('date_added'),
         'exercise_added_by': request.form.get('exercise_added_by'),
+        'image': base64.b64encode(request.files['image'].read()).decode("utf-8")
     })
     # redirect to exercises page after updating exercises
     return redirect(url_for('exercises'))
