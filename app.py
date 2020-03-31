ed7d23bc8448d0f47128c8db11408e4675f35ce8
Ivar Saris <ivarsaris@gmail.com>
2020-03-31 02:57:06 +0000

    made comments in app.py more descriptive

--- app.py
+++ app.py
@@ -14,89 +14,148 @@ app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 
 mongo = PyMongo(app)
 
-# open index.html
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# open about.html
 @app.route('/about')
 def about():
     return render_template('about.html')
 
 # add.html ##########################
 
-# route to template
 @app.route('/add')
 def add():
-    # opens add.html
     return render_template('add.html')
 
-# add exercise to database
 @app.route('/submit_exercise', methods=['POST'])
 def submit_exercise():
-    # get image from form
+    """
+    Add exercise to the database
+    """
     image = request.files['image']
+    
     # turn image into string
     image_string = base64.b64encode(image.read()).decode("utf-8")
-    # saves values in variable
+
     values = request.form.to_dict()
+    
     # turn type_of_exercise value to list so it can store multiple inputs
     values["type_of_exercise"] = request.form.getlist("type_of_exercise")
+    
     # encode image string to save in mongo
     values["image"] = "data:image/png;base64," + image_string
-    # define mongo database
+
+    """
+    check if user added valid name, needs to be longer than 3
+    and shorter than 30 characters. If not, page is reloaded with
+    warning message
+    """
+    name = request.form.get("name")
+
+    if len(name) < 3 or len(name) > 30:
+        flash("Please submit a name of more than 3, and less than 30 characters.", "warning")
+        return redirect(url_for('add'))
+
+    """
+    Check is user added description of more than 10 characters.
+    If not, page is reloaded with warning message 
+    """
+    description = request.form.get("description")
+
+    if len(description) <= 10:
+        flash("Please add a description of more than 10 characters to the exercise", "warning")
+        return redirect(url_for('add'))
+
+    """
+    Check if user added a type of exercise.
+    If not, page is reloaded with warning message 
+    """
+    type_of_exercise = request.form.getlist("type_of_exercise")
+
+    if len(type_of_exercise) < 1:
+        flash("Please choose one or more skill types.", "warning")
+        return redirect(url_for('add'))
+    
+    """
+    Check if user added a skill level.
+    If not, page is reloaded with warning message 
+    """
+    skill_level = request.form.get("skill_level")
+
+    if not skill_level:
+        flash("Please select a difficulty level", "warning")
+        return redirect(url_for('add'))
+
+    """
+    Check if user added a data.
+    If not, page is reloaded with warning message 
+    """
+    date_added = request.form.get("date_added")
+
+    if not date_added:
+        flash("Please select a date.", "warning")
+        return redirect(url_for('add'))
+
+    """
+    Check if user added their name.
+    If not, page is reloaded with warning message
+    """
+    user_name = request.form.get("exercise_added_by")
+
+    if not user_name:
+        flash("Please give your name.", "warning")
+
     exercises = mongo.db.exercises
-    # insert document in database with button in add.html
+
     exercises.insert_one(values)
-    # return to exercises page
 
-    # name = request.form.get("name")
-
-    # if not len(name) > 1:
-    #     flash("Please submit a name for the exercise.")
-    #     return redirect(request.url)
-
-    # flash("Exercise succesfully submitted to the database!")
-    # return redirect(request.url)
-
-    # return render_template(url_for('exercises'))
+    flash("Exercise succesfully submitted to the database!", "success")
+    
+    return redirect(url_for('exercises'))
 
 # exercises.html #########################
 
 @app.route('/exercises')
 def exercises():
-    # define exercises as all exercises in mongo collection
+    """
+    open exercises page and retrieve all
+    exercises from the database
+    """
     exercises = mongo.db.exercises.find()
-    # opens exercises page with exercises defined
+    
     return render_template('exercises.html', exercises=exercises)
 
-# route for filter exercises function
 @app.route('/exercises',  methods=['POST'])
-# filter exercises function
 def filter_exercises():
-    # define exercises as all exercises in mongo collection
+    """
+    Filter the exercises by type of exercise. Retrieve all
+    exercises and check which ones have a certain 'type'.
+    return these in the exercises template
+    """
     exercises = mongo.db.exercises
-    # get value from filter form in exercises.html
     type_filter = request.form.get('type_filter')
-    # check database if there are results with this value
-    try:
-        doc = exercises.find({'type_of_exercise': type_filter})
-    # error if can't connect to database
-    except:
-        print("Error accessing database!")
-    # if there are results, return them in exercises template
-    if doc:
-        return render_template('exercises.html', exercises=doc)
+
+    """
+    If a filter returns 0 results, open exercises template
+    and display message
+    """
+    doc = exercises.find({'type_of_exercise': type_filter})
+    if doc.count() == 0:
+        flash("No exercises of this type", "warning")
+        return redirect(url_for('exercises'))
+
+    return render_template('exercises.html', exercises=doc)
 
 # viewexercise.html #################################
 
 @app.route('/view_exercise/<exercise_id>')
 def view_exercise(exercise_id):
-    # find the particular exercise in the database using its ID
+    """
+    find the particular exercise in the database using its ID
+    """
     the_exercise = mongo.db.exercises.find_one({"_id": ObjectId(exercise_id)})
-    print(the_exercise)
-    # opens exercise in view_exercise.html with button in exercises.html
+    
     return render_template('viewexercise.html', exercise=the_exercise)
 
 # editexercise.html #################################
@@ -104,21 +163,23 @@ def view_exercise(exercise_id):
 # opens editexercise.html with 'edit exercise' button in viewexercise.html
 @app.route('/edit_exercise/<exercise_id>')
 def edit_exercise(exercise_id):
-    # define specific exercise with its Mongo ID
+    """
+    open the edit exercise template with a specific exercises' 
+    information using its mongo ID
+    """
     the_exercise = mongo.db.exercises.find_one({"_id": ObjectId(exercise_id)})
-    # return edit template with info of the exercise filled out
+
     return render_template('editexercise.html', exercise=the_exercise)
 
-# route for update exercise function
 @app.route('/update_exercise/<exercise_id>', methods=["POST"])
-# update exercise function
 def update_exercise(exercise_id):
-    # retrieve exercises from the database
+    """
+    open the edit exercise template with a specific exercises' 
+    information using its mongo ID
+    """
     exercises = mongo.db.exercises
-    # add document to database with form
     exercise = {
-        # all variables to be filled out in the form
-        'name': request.form.get('name'),
+        'name': request.form.get('name'),        
         'description': request.form.get('description'),
         'type_of_exercise': request.form.getlist('type_of_exercise'),
         'skill_level': request.form.get('skill_level'),
@@ -126,32 +187,36 @@ def update_exercise(exercise_id):
         'exercise_added_by': request.form.get('exercise_added_by')
         }
     existing_exercise = mongo.db.exercises.find_one({"_id": ObjectId(exercise_id)})
-    # check if user oploaded a new image or not
+    """
+    check if user oploaded a new image or not. if they did,
+    upload new picture, if not, keep using existing image.
+    """
     if request.files['image'].filename != '':
-        # save new image
         image = request.files['image']
-        # turn image into string
+        # code image into string to save it to database
         image_string = base64.b64encode(image.read()).decode("utf-8")
-        # save new image to database
         exercise['image'] = "data:image/png;base64," + image_string
     else:
-        # keep using existing image if no new image is given
         exercise['image'] = existing_exercise['image']
-    # add document to database with form
+
     exercises.replace_one({"_id": ObjectId(exercise_id)}, exercise)
-    # redirect to exercises page after updating exercises
+
+    flash("Exercise updated successfully", "success")
+
     return redirect(url_for('exercises'))
 
-# route for delete exercise function
 @app.route('/delete_exercise/<exercise_id>')
-# delete exercise function
 def delete_exercise(exercise_id):
-    # define exercises
+    """  
+    delete exercise from database with button
+    in edit exercise page.
+    """ 
     exercises = mongo.db.exercises
-    # delete exercise from database with button
     exercises.remove({"_id": ObjectId(exercise_id)
                       })
-    # open exercises page after deleting exercise
+
+    flash("Exercise succesfully deleted from the database.", "success")
+    
     return redirect(url_for('exercises'))
 
 # stats.html ######################################
@@ -159,21 +224,22 @@ def delete_exercise(exercise_id):
 # opens stats page
 @app.route('/stats')
 def stats():
-    # define the exercises in the database
     exercises = mongo.db.exercises
-    # count amount of exercises
     amount_of_exercises = exercises.count()
-    # sort exercises by date added, return in descending order
+    """
+    sort exercises by date added, return in ascending order.
+    find exercise last added
+    """
     exerc_sorted_date = exercises.find().sort("date_added", -1)
-    # find exercise last added
     last_aded_exerc = exerc_sorted_date[0]
-    # render template with the variables
+
     return render_template('stats.html', amount_of_exercises=amount_of_exercises,
                            last_aded_exerc=last_aded_exerc)
 
 
 # opens port for browser
 if __name__ == "__main__":
+    app.secret_key = 'pooldrillzqwerty'
     app.run(host="0.0.0.0",
             port=int("8080"),
             debug=True)
