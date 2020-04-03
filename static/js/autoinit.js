abcc299e0824bb6acaacebc151f8f8f2880e2bf3
Ivar Saris <ivarsaris@gmail.com>
2020-04-03 01:19:42 +0000

    fixed mistake causing error in console

--- static/js/autoinit.js
+++ static/js/autoinit.js
@@ -1,12 +1,3 @@
-// $(document).ready(function () {
-//     $('select').material_select();
-//     M.AutoInit();
-// });
-// init materialize tab
-    //  var elem = $('.tabs')
-    //  var options = {}
-    //  var instance = M.Tabs.init(elem, options);
-
 // initialize form select elements
 $(document).ready(function(){
     $('select').formSelect();
